192cebe2ee8139f3639413a38cbdbbfea05faa02
Kevin Romero <rowanatkinson23@yahoo.com>
2019-10-08 12:29:13 -0700

    image manipulator

--- components/LoginComponent.js
+++ components/LoginComponent.js
@@ -5,6 +5,7 @@ import { createBottomTabNavigator } from 'react-navigation'
 import * as SecureStore from 'expo-secure-store' //old version import { SecureStore } from 'expo';
 import * as Permissions from 'expo-permissions'; //import { Permissions, Notifications } from 'expo';
 import * as ImagePicker from 'expo-image-picker';
+import * as ImageManipulator from 'expo-image-manipulator';
 import { baseUrl } from '../shared/baseUrl';
 
 class LoginTab extends Component {
@@ -145,9 +146,22 @@ class RegisterTab extends Component {
             });
             if (!capturedImage.cancelled) {
                 console.log(capturedImage);
-                this.setState({ imageUrl: capturedImage.uri });
+                this.processImage(capturedImage.uri);
             }
         }
+
+    }
+
+    processImage = async (imageUri) => {
+        let processedImage = await ImageManipulator.manipulateAsync(
+            imageUri,
+            [
+                { resize: { width: 400 } }
+            ],
+            { format: 'png' }
+        );
+        console.log(processedImage);
+        this.setState({ imageUrl: processedImage.uri });
 
     }
 
